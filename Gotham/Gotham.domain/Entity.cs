a2f458e311e350fc2b025007f3852c2bfe0cef54
William Lecours <54285168+wlecours@users.noreply.github.com>
2019-12-10 12:14:16 -0500

    gitignore

---
+++ Gotham/Gotham.domain/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gotham.domain
+{
+    public abstract class Entity
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
---
+++ Gotham/Gotham.web.acceptanceTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Gotham.web.acceptanceTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
