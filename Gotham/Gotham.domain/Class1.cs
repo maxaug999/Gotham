b9c983625a4ad0050555cd641e517c0774cd04fc
William Lecours <54285168+wlecours@users.noreply.github.com>
2019-12-10 13:42:23 -0500

    delete

--- Gotham/Gotham.domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Gotham.domain
-{
-    public class Class1
-    {
-    }
-}
--- Gotham/Gotham.persistence/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Gotham.persistence
-{
-    public class Class1
-    {
-    }
-}
